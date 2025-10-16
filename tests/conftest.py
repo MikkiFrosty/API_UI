d8ceec4745e15b5d69466b49b94301ebe95a5af3
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-17 00:41:35 +0300

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
-
 import os
 import pytest
 from selene import browser
-from api_ui_cart_project.api.client import HttpClient
+from selenium import webdriver
 
-@pytest.fixture(scope="session", autouse=True)
-def base_url():
-    browser.config.base_url = os.getenv("BASE_URL", "https://demowebshop.tricentis.com")
-    return browser.config.base_url
 
-@pytest.fixture(scope="session")
-def client(base_url):
-    return HttpClient(base_url=base_url)
+@pytest.fixture(scope='session', autouse=True)
+def setup_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-dev-tools')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1366,768')
+
+    browser.config.driver_options = options
+    browser.config.base_url = os.getenv('BASE_URL', 'https://demowebshop.tricentis.com')
+    browser.config.timeout = 10
+
+    yield
+    browser.quit()
