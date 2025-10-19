3de85a4b7637b3220ecbddc4de3a6cb05321f57f
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-19 13:37:02 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import os
 import pytest
 from selene import browser
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_browser():
@@ -10,11 +11,15 @@ def setup_browser():
     options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-dev-tools')
-    options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1366,768')
 
-    browser.config.driver_options = options
+    cache_path = os.getenv('WDM_CACHE', '.wdm-cache')
+    os.makedirs(cache_path, exist_ok=True)
+
+    driver_path = ChromeDriverManager(path=cache_path, cache_valid_range=0).install()
+    service = Service(driver_path)
+    browser.config.driver = webdriver.Chrome(service=service, options=options)
+
     browser.config.base_url = os.getenv('BASE_URL', 'https://demowebshop.tricentis.com')
     browser.config.timeout = 10
 
