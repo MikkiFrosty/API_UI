9d888a5a740742535fdce41ff686677040f08e3d
SKropotova <Kropotova.S.M@yandex.ru>
2025-10-20 01:26:57 +0300

    fix path user

--- tests_api/test_reqres.py
+++ tests_api/test_reqres.py
@@ -4,6 +4,7 @@ import allure
 from jsonschema import validate
 from schemas import create_user, update_user, list_users, single_user, error_schema
 import urllib3
+import os
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -27,7 +28,8 @@ def test_post_create_user_schema_from_file(api_client):
     payload = {"name": "morpheus", "job": "leader"}
     r = api_client.post("/users", verify=False, json_body=payload)
     assert r.status_code == 201
-    with open("post_users.json", encoding="utf-8") as f:
+    SCHEMA_FILE = os.path.join(os.path.dirname(__file__), 'post_users.json')
+    with open(SCHEMA_FILE, encoding='utf-8') as f:
         validate(r.json(), create_user)
 
 def test_post_register_missing_password(api_client):
